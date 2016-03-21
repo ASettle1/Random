a7440bddee7d389d26424a41d138bb1679ed652f
asettle1 <asettle1@students.kennesaw.edu>
2016-03-21 15:20:30 -0400

    Made numerous revisions. This should be a working

--- src/assignment8/Assignment8.java
+++ src/assignment8/Assignment8.java
@@ -3,78 +3,120 @@ package assignment8;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+/**
+ *
+ * @author Aaron Settle
+ */
 public class Assignment8 {
-    
+
+    public static double totalGrossPay = 0;
+    public static double totalNetPay = 0;
     public static boolean repeat = true;
-    public static int weekCounter = 0;
+    public static int weekCounter = 1;
     public static Scanner scan = new Scanner(System.in);
-    PayCalculation pay = new PayCalculation();
-    public int[] values = new int[4];
-    
+    private static double[] payValues = new double[5];
+    private static String[] totalValues = new String[7];
+    private static PayCalculation pay = new PayCalculation();
+
     public static void main(String[] args) {
-        System.out.println("Would you like to calculate your pay for a week? Press Y (Yes) or N (No).");
-        while(repeat){
-                String input = scan.next();
-                input = input.toUpperCase();
-                switch (input) {
-                    case "Y":
-                        repeat = false;
-                        break;
-                    case "N":
+        while (repeat) {
+            System.out.println("Would you like to calculate your weekly pay? Press Y (Yes) or N (No).");
+            String input = scan.next();
+            input = input.toUpperCase();
+            switch (input) {
+                case "Y":
+                    repeat = false;
+                    /**
+                     * @param payValues: Store values entered by user to be used for the next set
+                     * of calculations include gross pay and net pay.
+                     */
+                    payValues[0] = validInt("Enter hours worked this week", 0, 60);
+                    payValues[1] = validDouble("Enter hourly wage", 0.00, 30.00);
+                    payValues[2] = validInt("Enter number of exemptions", 0, 5);
+                    payValues[3] = pay.calculateGrossPay(payValues[1], (int) payValues[0]);
+                    payValues[4] = (payValues[3] - (pay.calculateFederalTax(payValues[3], (int) payValues[2]) + pay.calculateStateTax(payValues[3], (int) payValues[2]) + pay.calculateSocialSecurity(payValues[3])));
+
+                    /** 
+                     * Store total amount of pay of both types based on all user inputs in a session.
+                     */
+                    totalGrossPay += payValues[3];
+                    totalNetPay += payValues[4];
+                    
+                    /**
+                     * @param totalValues - stores all calculation results to be displayed.
+                     * Perform calculations on entered values and store the results in order of:
+                     * Week Number, Hours, Gross Pay, Federal Tax, State Tax, Social Security, Net Pay
+                     */
+                    totalValues[0] = String.format("Week %d:", weekCounter);
+                    totalValues[1] = String.format("You worked %.2f hours this week at $%.2f per hour.", payValues[0], payValues[1]);
+                    totalValues[2] = String.format("Gross Pay: %.2f", payValues[3]);
+                    totalValues[3] = String.format("Federal Tax: $%.2f", pay.calculateFederalTax(payValues[3], (int) payValues[2]));
+                    totalValues[4] = String.format("State Tax: $%.2f", pay.calculateStateTax(payValues[3], (int) payValues[2]));
+                    totalValues[5] = String.format("Social Security: $%.2f", pay.calculateSocialSecurity(payValues[3]));
+                    totalValues[6] = String.format("Net Pay: $%.2f", payValues[4]);
+                    weekCounter++;
+
+                    for (String s : totalValues) {
+                        System.out.println(s);
+                    }
+
+                    repeat = true;
+                    break;
+                case "N":
+                    if (weekCounter > 1) {
+                        System.out.printf("Total Gross pay: $%.2f\n", totalGrossPay);
+                        System.out.printf("Total Net pay: $%.2f\n", totalNetPay);
+                        barGraph(weekCounter);
                         System.exit(0);
-                        break;
-                    default:
-                        System.out.println("Please enter Y (Yes) to continue or N (No) to quit.");
-                        break;
-                }
+                    } else {
+                        System.exit(0);
+                    }
+                    break;
+                default:
+                    break;
+            }
         }
     }
-    
-    public static int validInt(String prompt, int lowerBound, int upperBound){
-        for(;;){
+
+    public static int validInt(String prompt, int lowerBound, int upperBound) {
+        for (;;) {
             System.out.printf("%s (%d-%d)\n", prompt, lowerBound, upperBound);
-            try
-            {
+            try {
                 int input = scan.nextInt();
-                if(input >= lowerBound && input <= upperBound){
+                if (input >= lowerBound && input <= upperBound) {
                     return input;
-                }
-                else{
+                } else {
                     System.out.println("Input was not within bounds. Please try again.");
                 }
-            }
-            catch(InputMismatchException e){
+            } catch (InputMismatchException e) {
                 System.out.println("Input was not a number. Please try again.");
                 scan.next();
             }
         }
     }
-    
-    public static double validDouble(String prompt, double lowerBound, double upperBound){
-        for(;;){
+
+    public static double validDouble(String prompt, double lowerBound, double upperBound) {
+        for (;;) {
             System.out.printf("%s (%.2f-%.2f)\n", prompt, lowerBound, upperBound);
-            try
-            {
+            try {
                 double input = scan.nextDouble();
-                if(input >= lowerBound && input <= upperBound){
+                if (input >= lowerBound && input <= upperBound) {
                     return input;
-                }
-                else{
+                } else {
                     System.out.println("Input was not within bounds. Please try again.");
                 }
-            }
-            catch(InputMismatchException e){
-               System.out.println("Input was not a number. Please try again.");
-               scan.next();
+            } catch (InputMismatchException e) {
+                System.out.println("Input was not a number. Please try again.");
+                scan.next();
             }
         }
     }
-    
-    public void barGraph(int numWeeks){
-        String stars = "*";
-        for(int i = 0; i < weekCounter; i++){
+
+    public static void barGraph(int numWeeks) {
+        String stars = "";
+        for (int i = 1; i < numWeeks; i++) {
             stars += "*";
         }
-        System.out.println("Amount of weeks calculated: " + stars);
+        System.out.printf("%s : %s\n", "Amount of pay weeks calculated", stars);
     }
 }
--- src/assignment8/PayCalculation.java
+++ src/assignment8/PayCalculation.java
@@ -7,9 +7,13 @@ package assignment8;
 public class PayCalculation {
     private static final double FEDERAL_EXEMPT = 55.77;
     private static final double STATE_EXEMPT = 38.46;
-    private static final double SS_PERCENT = 7.65;
+    private static final double SS_PERCENT = .0765;
     
-    private double calculateGrossPay(double wage, int hoursWorked){
+    protected double calculateGrossPay(double wage, int hoursWorked){
+        /** Calculate Gross Pay
+         *  If the total amount of hours worked is less than 40, wage is gross pay is wage * hours.
+         *  If hours worked exceeds 40, overtime of (wage * 1.5) is applied to all hours over 40 along with base pay of (wage * 40).
+         */
         if(hoursWorked <= 40){
             return(wage * hoursWorked);
         }
@@ -20,13 +24,21 @@ public class PayCalculation {
         }
     }
     
-    private double calculateFederalTax(double grossPay, int numExemptions){
-        //@param    taxEligible is your pay gross pay after accounting for exemptions
-        //@return   Amount of federal tax withholding after calculating tax bracket and taxable income
+    protected double calculateFederalTax(double grossPay, int numExemptions){
+        /** Calculate Federal Tax
+        * 
+        *@param     taxEligible is the amount of taxable income earned after federal exemptions are subtracted.
+        *@return    Calculated amount of tax be paid based on income, federal exemption, and corresponding tax bracket.
+        * 
+        */
         double taxEligible = grossPay - (FEDERAL_EXEMPT * numExemptions);
         
         
-        //If I had more time, I could probably refactor this into a map
+        /**
+         * Tax bracket calculation:
+         * There are 6 total tax brackets from $0 to $5750+.
+         * I could probably refactor this into a map if I had additional time.
+         */
         if(taxEligible < 51.00){
             return 0;
         }
@@ -47,7 +59,11 @@ public class PayCalculation {
         }
     }
     
-    private double calculateStateTax(double grossPay, int numExemptions){
+    protected double calculateStateTax(double grossPay, int numExemptions){
+        /** Calculate State Tax
+         * 
+         *@return If pay gross pay exceeds exemption amount, 3% of gross pay after exemptions is taxed.
+         */
         if(grossPay <= (STATE_EXEMPT * numExemptions)){
             return 0;
         }
@@ -56,7 +72,11 @@ public class PayCalculation {
         }
     }
     
-    private double calculateSocialSecurity(double grossPay){
+    protected double calculateSocialSecurity(double grossPay){
+        /** Calculate Social Security
+         * 
+         *@return   Social Security withholding is taxed by a fixed 7.65% against gross pay.
+         */
         return(SS_PERCENT * grossPay);
     }
 }
